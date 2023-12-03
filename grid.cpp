40212f0bfa36ca3bf8a0bc54b3f5a439567852c0
Mates Vrba <xvrbam03@stud.fit.vutbr.cz>
2023-12-03 09:44:09 +0100

    refactor: fix new grid function

--- grid.cpp
+++ grid.cpp
@@ -6,25 +6,20 @@ using namespace std;
 
 //grid start 0,0,0 is as top left near corner, so coordinates (W,L,H) is at right bottom far corner
 
-
-// Cell getUpdatedCell(int64_t x,int64_t y,int64_t z)
-// {
-
-// }
-
-vector<vector<vector<Cell>>> Grid::getNewGrid()
+vec3d<Cell> Grid::getNewGrid()
 {
-
+	future_grid.resize(width);
     //tady bych chtel ten deep copy
-
     for (int64_t i = 0; i < width; ++i) {
+		future_grid[i].resize(length);
         for (int64_t j = 0; j < length; ++j) {
+			future_grid[i][j].resize(height);
             for (int64_t k = 0; k < height; ++k) {
-                // future_grid[i][j][k] = current_grid.getUpdatedCell(i,j,k);
-                void();
+                future_grid[i][j][k] = Cell(i,j,k);
             }
         }
     }
+	return {};
 }
 
 Grid::Grid(int64_t width,int64_t length,int64_t height)
@@ -41,5 +36,17 @@ Grid::Grid(int64_t width,int64_t length,int64_t height)
 				current_grid[i][j][k] = Cell(i,j,k);
 			}
 		}
+	}
+}
+
+void Grid::print(const vec3d<Cell>& grid, std::function<float(const Cell&)> getVal){
+	for(auto& row: grid){
+		for(auto& col: row){
+			for(auto& cell: col){
+				cout << getVal(cell) << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
 	}
 }
--- grid.hpp
+++ grid.hpp
@@ -1,23 +1,31 @@
 #pragma once
 #include "cell.hpp"
 #include <vector>
+#include <functional>
 
 using namespace std;
+
+template<typename T>
+using vec3d = vector<vector<vector<T>>>;
 
 class Grid {
 public:
     Grid(int64_t width,int64_t length,int64_t height);
     Cell getUpdatedCell(int64_t x,int64_t y,int64_t z);
+	vec3d<Cell> getNewGrid();
 
-public:
+	void print(const vec3d<Cell>& grid, std::function<float(const Cell&)>);
+
+	vec3d<Cell> const &getCurrent(){return current_grid;}
+	vec3d<Cell> const &getFuture(){return future_grid;}
 
 
-    vector<vector<vector<Cell>>> current_grid;
-    vector<vector<vector<Cell>>> future_grid;
+private:
+    vec3d<Cell> current_grid;
+    vec3d<Cell> future_grid;
     int width;
     int length;
     int height;
     int time; //generetion number
 
-    vector<vector<vector<Cell>>> getNewGrid();
 };
